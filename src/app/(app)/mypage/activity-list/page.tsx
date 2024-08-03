5cafe09724df00b954e692f7f5b0c28804d7102f
leesojeong <sjlee8969@gmail.com>
2024-08-03 21:15:40 +0900

    feat: 마이페이지 레이아웃 메뉴 제작

---
+++ src/app/(app)/mypage/activity-list/page.tsx
@@ -0,0 +1,14 @@
+const ActivityList: React.FC = () => {
+  return (
+    <div>
+      <h1 className="">예약 내역</h1>
+      <p className="">리스트</p>
+      <p className="">리스트</p>
+      <p className="">리스트</p>
+      <p className="">리스트</p>
+      <p className="">리스트</p>
+    </div>
+  );
+};
+
+export default ActivityList;
---
+++ src/app/components/SideNavigation/SideNavigation.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import NavItem from "./NavItem";
+import ProfileImageUploader from "./ProfileImageUploader";
+import Account from "@icons/icon_account.svg";
+import Setting from "@icons/icon_my_activities.svg";
+import Calendar from "@icons/icon_my_activities_reservations.svg";
+import Reservation from "@icons/icon_my_reservations.svg";
+
+const navItems = [
+  { href: "/mypage/me", label: "내 정보", icon: Account },
+  { href: "/mypage/reservations", label: "예약내역", icon: Reservation },
+  { href: "/mypage/activity-list", label: "내 체험 관리", icon: Setting },
+  { href: "/mypage/activity-status", label: "예약현황", icon: Calendar },
+];
+
+const SideNavigation: React.FC = () => {
+  return (
+    <div className="shadow-[rgba(17, 34, 17, 0.05)] rounded-xl border border-gray-300 p-6 shadow-md">
+      <div className="mb-4">
+        <ProfileImageUploader />
+      </div>
+      <nav>
+        <ul className="flex flex-col gap-2">
+          {navItems.map((item, index) => (
+            <NavItem
+              key={index}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default SideNavigation;
