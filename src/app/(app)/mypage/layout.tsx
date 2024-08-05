b0d3c8f4d29ef47d4de6f819e28bb4d0b0c4bb2a
leesojeong <sjlee8969@gmail.com>
2024-08-05 14:46:09 +0900

    fix: 오타수정(2x) 및 모바일 시 사이드 메뉴 hidden 처리

--- src/app/(app)/mypage/layout.tsx
+++ src/app/(app)/mypage/layout.tsx
@@ -6,8 +6,8 @@ export default function MyPageLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="container 2x flex flex-1 flex-wrap gap-6">
-      <div className="w-full md:w-4/12 xl:w-96">
+    <div className="container flex flex-1 flex-wrap gap-6">
+      <div className="hidden w-full md:block md:w-4/12 xl:w-96">
         <SideNavigation></SideNavigation>
       </div>
       <div className="flex-1">{children}</div>
